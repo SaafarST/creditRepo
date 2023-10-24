df53f531d08342783d1d1a967bdd53de570178dd
TohirJu <53301540+SaafarST@users.noreply.github.com>
2023-10-24 23:55:58 +0500

    4rd commit: Updating CustomerOwning class

--- creditScoringAP/src/main/java/org/credirScore/CreditScoringSystem.java
+++ creditScoringAP/src/main/java/org/credirScore/CreditScoringSystem.java
@@ -6,12 +6,6 @@ public class CreditScoringSystem {
 
     public static void main(String[] args) {
         Customer customer = new Customer();
-//        customer.setIncome(50000);
-//        customer.setEmploymentYears(5);
-//        customer.setCreditHistoryScore(700);
-//
-//        int creditScore = calculateCreditScore(customer);
-//        System.out.println("Credit Score: " + creditScore);
     }
 }
 
